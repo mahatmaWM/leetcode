64c6beadf98253bb28bd96500fec275b96a8da83
chrismwang <chrismwang@tencent.com>
2020-05-15 20:48:12 +0800

    update

---
+++ leetcode/editor/cn/842.将数组拆分成斐波那契序列.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=842 lang=python3
+#
+# [842] 将数组拆分成斐波那契序列
+#
+
+# @lc code=start
+class Solution:
+    def splitIntoFibonacci(self, S: str) -> List[int]:
+        
+# @lc code=end
+
---
+++ leetcode/editor/cn/846.一手顺子.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=846 lang=python3
+#
+# [846] 一手顺子
+#
+
+# @lc code=start
+class Solution:
+    def isNStraightHand(self, hand: List[int], W: int) -> bool:
+# @lc code=end
+
