9331c3de1ad08cd084bdcf69e04520b37773741a
chrismwang <chrismwang@tencent.com>
2020-06-15 21:06:24 +0800

    update

---
+++ leetcode/editor/cn/410.分割数组的最大值.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode.cn id=410 lang=python3
+#
+# [410] 分割数组的最大值
+#
+# https://leetcode-cn.com/problems/split-array-largest-sum/description/
+#
+# algorithms
+# Hard (43.21%)
+# Likes:    169
+# Dislikes: 0
+# Total Accepted:    6.9K
+# Total Submissions: 16K
+# Testcase Example:  '[7,2,5,10,8]\n2'
+#
+# 给定一个非负整数数组和一个整数 m，你需要将这个数组分成 m 个非空的连续子数组。设计一个算法使得这 m 个子数组各自和的最大值最小。
+#
+# 注意:
+# 数组长度 n 满足以下条件:
+#
+#
+# 1 ≤ n ≤ 1000
+# 1 ≤ m ≤ min(50, n)
+#
+#
+# 示例:
+#
+#
+# 输入:
+# nums = [7,2,5,10,8]
+# m = 2
+#
+# 输出:
+# 18
+#
+# 解释:
+# 一共有四种方法将nums分割为2个子数组。
+# 其中最好的方式是将其分为[7,2,5] 和 [10,8]，
+# 因为此时这两个子数组各自的和的最大值为18，在所有情况中最小。
+#
+# 解法：
+# 最小和必然落在 [max(nums), sum(nums)] 之间
+# 我们可以使用二分来进行查找
+#
+
+
+# @lc code=start
+class Solution:
+
+    def splitArray(self, nums: List[int], m: int) -> int:
+        # 由于题目的返回要求：返回最小和的值
+        # 最小和必然落在 [max(nums), sum(nums)] 之间
+        # 我们可以使用二分来进行查找
+        low, high = max(nums), sum(nums)
+
+        while low < high:
+            mid = (low + high) // 2
+
+            # 淘汰算法
+            # 我们由前向后对nums进行划分，使其子数组和 <= mid，然后统计满足条件的数组数量
+            # 若我们选的sum值过小，则满足条件的数量 > m，目标值应落在 [mid+1, high]
+            # 若我们选的sum值过大，则满足条件的数量 < m，目标值应落在 [low, mid-1]
+            count = 0
+            sub_sum = 0
+            for i in range(len(nums)):
+                sub_sum += nums[i]
+                if sub_sum > mid:
+                    count += 1
+                    sub_sum = nums[i]
+
+            # 注意：末尾还有一个子数组我们没有统计，这里把它加上
+            count += 1
+            if count > m:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+
+# @lc code=end
---
+++ leetcode/editor/cn/424.替换后的最长重复字符.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode.cn id=424 lang=python3
+#
+# [424] 替换后的最长重复字符
+#
+# https://leetcode-cn.com/problems/longest-repeating-character-replacement/description/
+#
+# algorithms
+# Medium (47.44%)
+# Likes:    114
+# Dislikes: 0
+# Total Accepted:    7.8K
+# Total Submissions: 16.4K
+# Testcase Example:  '"ABAB"\n2'
+#
+# 给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k
+# 次。在执行上述操作后，找到包含重复字母的最长子串的长度。
+#
+# 注意:
+# 字符串长度 和 k 不会超过 10^4。
+#
+# 示例 1:
+#
+# 输入:
+# s = "ABAB", k = 2
+#
+# 输出:
+# 4
+#
+# 解释:
+# 用两个'A'替换为两个'B',反之亦然。
+#
+#
+# 示例 2:
+#
+# 输入:
+# s = "AABABBA", k = 1
+#
+# 输出:
+# 4
+#
+# 解释:
+# 将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+# 子串 "BBBB" 有最长重复字母, 答案为 4。
+#
+# 满足条件 -> 扩张长度1，不满足条件 -> 滑动
+# 保存滑动窗口内相同字母出现次数的历史最大值，
+# 通过判断窗口宽度(right - left + 1) - maxFreq > K来决定窗口是否做滑动，否则窗口就扩张
+# 这里只需要记录历史最大值的原因是我们只需要最终保证输出的是最大长度，无需缩减窗口大小
+#
+
+# @lc code=start
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        from collections import defaultdict
+        left = 0
+        # cur存放窗口中某个字符出现的次数
+        cur = defaultdict(int)
+        res = 0
+        for right, val in enumerate(s):
+            cur[val] += 1
+            # 找到目前最大字符个数,看该窗口是否多余翻转k个字符
+            while right - left + 1 - max(cur.values()) > k:
+                cur[s[left]] -= 1
+                left += 1
+            res = max(res, right - left + 1)
+        return res
+
+# @lc code=end
+
