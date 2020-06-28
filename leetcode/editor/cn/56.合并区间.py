f0edd78e17fbeaddd50aafedba4766cca89bceba
chrismwang <chrismwang@tencent.com>
2020-06-28 13:50:51 +0800

    update

--- leetcode/editor/cn/56.合并区间.py
+++ leetcode/editor/cn/56.合并区间.py
@@ -1,3 +1,18 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+#
+# [56] 合并区间
+#
+# https://leetcode-cn.com/problems/merge-intervals/description/
+#
+# algorithms
+# Medium (42.68%)
+# Likes:    459
+# Dislikes: 0
+# Total Accepted:    104.9K
+# Total Submissions: 245.8K
+# Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+#
 # 给出一个区间的集合，请合并所有重叠的区间。
 #
 # 示例 1:
@@ -7,37 +22,32 @@
 # 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
 #
 #
-# 示例 2:
+# 示例 2:
 #
 # 输入: [[1,4],[4,5]]
 # 输出: [[1,5]]
 # 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
-# Related Topics 排序 数组
+#
+#
 
-
-# leetcode submit region begin(Prohibit modification and deletion)
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
         res = []
         intervals.sort(key=lambda x: x[0])
 
-        tmp_inter = intervals[0]
+        curr = intervals[0]
         for i in range(1, len(intervals)):
-            if tmp_inter[1] < intervals[i][0]:
-                res.append(tmp_inter)
-                tmp_inter = intervals[i]
+            # 如果curr的end比新的start小，则保存一个结果，更新curr
+            if curr[1] < intervals[i][0]:
+                res.append(curr)
+                curr = intervals[i]
+            # 更新curr的end值
             else:
-                tmp_inter[1] = max(intervals[i][1], tmp_inter[1])
-        res.append(tmp_inter)
+                curr[1] = max(intervals[i][1], curr[1])
+        res.append(curr)
         return res
+# @lc code=end
 
-
-# leetcode submit region end(Prohibit modification and deletion)
-if __name__ == "__main__":
-    print(Solution().merge(intervals=[[1, 3], [2, 6], [8, 10], [15, 18]]))
---
+++ leetcode/editor/cn/986.区间列表的交集.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=986 lang=python3
+#
+# [986] 区间列表的交集
+#
+# https://leetcode-cn.com/problems/interval-list-intersections/description/
+#
+# algorithms
+# Medium (63.43%)
+# Likes:    58
+# Dislikes: 0
+# Total Accepted:    6.7K
+# Total Submissions: 10.5K
+# Testcase Example:  '[[0,2],[5,10],[13,23],[24,25]]\n[[1,5],[8,12],[15,24],[25,26]]'
+#
+# 给定两个由一些 闭区间 组成的列表，每个区间列表都是成对不相交的，并且已经排序。
+#
+# 返回这两个区间列表的交集。
+#
+# （形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <=
+# b。两个闭区间的交集是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3]。）
+#
+#
+#
+# 示例：
+#
+#
+#
+# 输入：A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+# 输出：[[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+#
+#
+#
+#
+# 提示：
+#
+#
+# 0 <= A.length < 1000
+# 0 <= B.length < 1000
+# 0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        i, j = 0, 0 # 双指针
+        res = []
+        while i < len(A) and j < len(B):
+            a1, a2 = A[i][0], A[i][1]
+            b1, b2 = B[j][0], B[j][1]
+            # 两个区间存在交集
+            if b2 >= a1 and a2 >= b1:
+                # 计算出交集，加入 res
+                res.append([max(a1, b1), min(a2, b2)])
+            # 指针前进
+            if b2 < a2:
+                j += 1
+            else:
+                i += 1
+        return res
+# @lc code=end
+
