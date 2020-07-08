e25398f030134ccaa97f119b3a6394d4b096af45
chrismwang <chrismwang@tencent.com>
2020-07-08 23:12:26 +0800

    update

--- leetcode/editor/cn/223.矩形面积.py
+++ leetcode/editor/cn/223.矩形面积.py
@@ -32,12 +32,11 @@
 class Solution:
     def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
         # 调整两个矩形位置, 让第一个矩形靠最左边
-        if A > E:
-            return self.computeArea(E, F, G, H, A, B, C, D)
+        if A > E: return self.computeArea(E, F, G, H, A, B, C, D)
         # 没有重叠的情况
-        if B >= H or D <= F or C <= E:
-            return abs(A - C) * abs(B - D) + abs(E - G) * abs(F - H)
-        # 重叠情况
+        if B >= H or D <= F or C <= E: return abs(A - C) * abs(B - D) + abs(E - G) * abs(F - H)
+
+        # 重叠情况，计算重叠部分的面积
         # 下边界
         down = max(A, E)
         # 上
